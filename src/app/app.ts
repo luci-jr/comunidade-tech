2e87b6235c846ddea4b2c99dc3b358e301de4e49
Lucivaldo <lucivaldojr25@gmail.com>
2026-02-26 15:58:32 -0300

    inicio de um projeto com intuito de ajudar quem está ccomeçando sua carreira em TI.

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
+
+@Component({
+  selector: 'app-root',
+  imports: [Cabecalho],
+templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('comunidade-tech');
+}
