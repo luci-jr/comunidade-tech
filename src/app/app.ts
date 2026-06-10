26f7e93452f80620926af7d6c0c53f613f3b25dc
Lucivaldo <lucivaldojr25@gmail.com>
2026-06-10 12:55:19 -0300

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,12 @@ import { Router, RouterOutlet } from '@angular/router';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Rodape } from './componentes/rodape/rodape';
 import { LoadingComponent } from './componentes/loading/loading.component';
-import { Login } from './componentes/paginas/login/login';
 import { LoginModalService } from './servicos/login-modal.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Cabecalho, Rodape, RouterOutlet, LoadingComponent, Login],
+  imports: [Cabecalho, Rodape, RouterOutlet, LoadingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
